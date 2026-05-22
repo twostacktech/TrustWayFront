ae16746cfaf726d8337a8058047f81a68a391a64
macedo <ds091339@gmail.com>
2026-05-22 15:37:26 -0300

    fix:rotas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AdmCliente from './pages/Cliente/AdmCliente'
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Apolices from "./pages/apolices/Apolices";
 
 function HomeLayout() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/" element={<HomeLayout />} />
         <Route path="/home" element={<HomeLayout />} />
         <Route path="/admcliente" element={<AdmCliente />} />
+        <Route path="/apolices" element={<Apolices/>} />
       </Routes>
     </BrowserRouter>
   );
